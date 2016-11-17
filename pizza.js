1894850d76f94770aebb154ff7d1cde15d7647c5
hugh88 <7hgallagly@stpatrick.org>
2016-11-17 10:55:19 -0600

    pizza
    started

---
+++ pizza.js
@@ -0,0 +1,20 @@
+class pizza(s,m,v){
+  this.size = s;
+  this.meatToppings = m;
+  this.veggieToppings = v;
+  //type construct below
+  sizeCost (){
+if(this.size = "Small")
+return sizecost = "7.99"
+
+
+  //type instance functions below
+
+
+
+
+
+  //type class functions below
+
+
+}
